41bc683355baea20930e3acdc4e19306c07a4fc1
Emilio Pomares <emilio.pomares.porras@gmail.com>
2022-11-27 20:17:50 +0200

    Added Script2 to Object1

---
+++ Assets/Script2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour
+{
+    public float FloatParameter = 9.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
